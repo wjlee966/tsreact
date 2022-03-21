25483fdc7227214d1ba7dafc694336bc349aa65e
wonjoonth <wjlee966@gmail.com>
2022-03-21 21:39:14 +0900

    08. input 상태 관리하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Counter from './Counter';
+// import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   return (
     <>
-      <Counter />
+      <InputSample />
     </>
   );
 }
