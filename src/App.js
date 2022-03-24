6257b174fff5d9b73cd6f23061c105af6b9eb795
wonjoonth <wjlee966@gmail.com>
2022-03-24 19:03:06 +0900

    18. useCallback 를 사용하여 함수 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import Counter from './Counter';
 // import InputSample from './InputSample';
-import { useMemo, useRef, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 
@@ -18,13 +18,16 @@ function App() {
   const nameInput = useRef();
   const { username, email } = inputs;
 
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
 
   const [users, setUsers] = useState([
     {
@@ -49,7 +52,7 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -64,26 +67,54 @@ function App() {
     nameInput.current.focus();
     console.log(nextId);
     nextId.current += 1;
-  };
+  }, [email, username, users]);
 
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  };
+  const onRemove = useCallback(
+    id => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  );
 
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id
-          ? {
-              ...user,
-              active: !user.active,
-            }
-          : user
-      )
-    );
-  };
+  // 이 함수들은 컴포넌트가 리렌더링 될 때 마다 새로 만들어집니다.
+  // 함수를 선언하는 것 자체는 사실 메모리도, CPU 도
+  // 리소스를 많이 차지 하는 작업은 아니기 때문에
+  // 함수를 새로 선언한다고 해서 그 자체 만으로 큰 부하가 생길일은 없지만,
+  // 한번 만든 함수를 필요할때만 새로 만들고 재사용하는 것은 여전히 중요합니다.
+
+  // 주의 하실 점은, 함수 안에서 사용하는 상태 혹은 props 가 있다면 꼭,
+  // deps 배열안에 포함시켜야 된다는 것 입니다.
+  // 만약에 deps 배열 안에 함수에서 사용하는 값을 넣지 않게 된다면,
+  // 함수 내에서 해당 값들을 참조할때 가장 최신 값을 참조 할 것이라고 보장 할 수 없습니다.
+  // props 로 받아온 함수가 있다면, 이 또한 deps 에 넣어주어야 해요.
+
+  // 사실, useCallback 은 useMemo 를 기반으로 만들어졌습니다.
+  // 다만, 함수를 위해서 사용 할 때 더욱 편하게 해준 것 뿐이지요.
+  // 이런식으로도 표현 할 수 있습니다.
+
+  // const onToggle = useMemo(
+  //   () => () => {
+  //     /* ... */
+  //   },
+  //   [users]
+  // );
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id
+            ? {
+                ...user,
+                active: !user.active,
+              }
+            : user
+        )
+      );
+    },
+    [users]
+  );
 
   // useMemo 의 첫번째 파라미터에는 어떻게 연산할지 정의하는 함수를 넣어주면 되고
   // 두번째 파라미터에는 deps 배열을 넣어주면 되는데,
