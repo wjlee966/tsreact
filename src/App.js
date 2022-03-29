eb68285d07ff25dbe3ca5284aaeb10c34ab0a18a
wonjoonth <wjlee966@gmail.com>
2022-03-29 19:36:06 +0900

    03. 서브라우트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -14,6 +14,9 @@ function App() {
         <li>
           <Link to='/about'>소개</Link>
         </li>
+        <li>
+          <Link to='/profiles'>프로필 목록</Link>
+        </li>
       </ul>
       <hr />
 
@@ -21,7 +24,7 @@ function App() {
       {/* <Route path="주소규칙" component={보여주고싶은 컴포넌트}> */}
       <Route path='/' component={Home} exact />
       <Route path='/about' component={About} />
-      <Route path='/profile/:username' component={Profile} />
+      <Route path='/profiles' component={Profiles} />
     </div>
   );
 }
---
+++ src/Profiles.js
@@ -0,0 +1,28 @@
+import { Route, Link } from 'react-router-dom';
+import Profile from './Profile';
+
+const Profiles = () => {
+  return (
+    <div>
+      <h3>사용자 목록</h3>
+      <ul>
+        <li>
+          <Link to='/profiles/velopert'>velopert</Link>
+        </li>
+        <li>
+          <Link to='/profiles/homer'>homer</Link>
+        </li>
+      </ul>
+
+      {/* 서브 라우트는, 라우트 내부의 라우트를 만드는것을 의미합니다. */}
+      {/* 그냥 컴포넌트를 만들어서 그 안에 또 Route 컴포넌트를 렌더링하시면 됩니다. */}
+
+      {/* 특정 라우트 내에 탭 같은것을 만들게 된다면,
+          단순히 state 로 관리하는 것 보다 서브 라우트로 관리를 하는 것을 권장드립니다. */}
+      <Route path='/profiles' exact render={() => <div>사용자를 선택해주세요!</div>} />
+      <Route path='/profiles/:username' component={Profile} />
+    </div>
+  );
+};
+
+export default Profiles;
