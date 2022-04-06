bf6078816ff4b1eae275d384a577c7f83ba3a577
wonjoonth <wjlee966@gmail.com>
2022-04-06 10:43:56 +0900

    03. redux-logger 사용 및 미들웨어와 DevTools 함께 사용

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; // 리덕스 개발자 도구
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
-import myLogger from './middlewares/myLogger';
+// import myLogger from './middlewares/myLogger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger))); // 여러개의 미들웨어를 적용 할 수 있습니다.
 
 ReactDOM.render(
   <React.StrictMode>
