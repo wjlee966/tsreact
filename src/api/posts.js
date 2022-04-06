f4b6e251b0f631fd045bbb74008483876e9780d1
wonjoonth <wjlee966@gmail.com>
2022-04-06 17:04:58 +0900

    16. CORS and Proxy

--- src/api/posts.js
+++ src/api/posts.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 
 // 포스트 목록을 가져오는 비동기 함수
 export const getPosts = async () => {
-  const res = await axios.get('http://localhost:4000/posts');
+  const res = await axios.get('/posts');
   return res.data;
 };
 
 // ID로 포스트를 조회하는 비동기 함수
 export const getPostById = async id => {
-  const res = await axios.get(`http://localhost:4000/posts/${id}`);
+  const res = await axios.get(`/posts/${id}`);
   return res.data;
 };
