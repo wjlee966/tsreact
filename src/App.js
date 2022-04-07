a2893ee146d31ae8c70de724eb60791c9e226837
wonjoonth <wjlee966@gmail.com>
2022-04-07 16:44:19 +0900

    18. redux-saga B - 기초 비동기 카운터 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route } from 'react-router-dom';
 import './App.css';
-// import CounterContainer from './containers/CounterContainer';
+import CounterContainer from './containers/CounterContainer';
 // import PostListContainer from './containers/PostListContainer';
 import PostListPage from './page/PostListPage';
 import PostPage from './page/PostPage';
@@ -11,6 +11,7 @@ function App() {
       {/* 페이지 (리액트 라우터) 적용 전 */}
       {/* <PostListContainer /> */}
       {/* 페이지 (리액트 라우터) 적용 후 */}
+      <CounterContainer />
       <Route path='/' component={PostListPage} exact />
       <Route path='/:id' component={PostPage} />
     </>
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
+import { all } from 'redux-saga/effects';
+import counter, { counterSaga } from './counter';
 import posts from './posts';
 
 const rootReducer = combineReducers({ counter, posts });
+
+export function* rootSaga() {
+  yield all([counterSaga()]); // all은 배열 안의 여러 사가를 동시에 실행시켜줍니다.
+}
 
 export default rootReducer;
