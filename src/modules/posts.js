6a5cda9e90680fd1d3396532361544946c317537
wonjoonth <wjlee966@gmail.com>
2022-04-07 17:30:19 +0900

    21. redux-saga E - 라우터 연동

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -5,7 +5,7 @@
 // 1. 프로미스가 시작, 성공, 실패했을때 다른 액션을 디스패치해야합니다.
 // 2. 각 프로미스마다 thunk 함수를 만들어 주어야 합니다.
 // 3. 리듀서에서 액션에 따라 로딩중, 결과, 에러 상태를 변경해 주어야 합니다.
-import { takeEvery } from 'redux-saga/effects';
+import { getContext, takeEvery } from 'redux-saga/effects';
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
 import {
   createPromiseSaga,
@@ -26,6 +26,7 @@ const GET_POSTS_ERROR = 'posts/GET_POSTS_ERROR'; // 요청 실패
 const GET_POST = 'posts/GET_POST'; // 요청 시작
 const GET_POST_SUCCESS = 'posts/GET_POST_SUCCESS'; // 요청 성공
 const GET_POST_ERROR = 'posts/GET_POST_ERROR'; // 요청 실패
+const GO_TO_HOME = 'posts/GO_TO_HOME';
 
 // 특정 포스트 조회시 재로딩 이슈 #1
 // 포스트 비우기
@@ -101,13 +102,21 @@ const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
 export function* postSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
 }
 
-export const goToHome =
-  () =>
-  (dispatch, getState, { history }) => {
-    history.push('/');
-  };
+// export const goToHome =
+//   () =>
+//   (dispatch, getState, { history }) => {
+//     history.push('/');
+//   };
+function* goToHomeSaga() {
+  const history = yield getContext('history');
+  history.push('/');
+}
+
+// 3번째 인자를 사용하면 withExtraArgument 에서 넣어준 값들을 사용할 수 있습니다.
+export const goToHome = () => ({ type: GO_TO_HOME });
 
 // 특정 포스트 조회시 재로딩 이슈 #1
 export const clearPost = () => ({ type: CLEAR_POST });
